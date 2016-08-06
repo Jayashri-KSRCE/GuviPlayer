57476154100c6c11a3b32cc0b4380e1021f05f9e
Jayashri <jayashri.mcse@ksrce.ac.in>
2016-08-06 11:03:21 +0530

    Create CheckSubString.java

---
+++ CheckSubString.java
@@ -0,0 +1,15 @@
+public class CheckSubString {
+
+	public static void main(String[] args) {
+		String s1="test12String";
+		String s2="123";
+		if(s1.contains(s2)) {
+			System.out.println("1");
+		}
+		else {
+			System.out.println("-1");
+		}
+
+	}
+
+}
