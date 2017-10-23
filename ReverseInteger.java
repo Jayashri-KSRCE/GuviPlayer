cfade6ba13f927c081934342795b8df1b39f9b54
Jayashri <jayashri.mcse@ksrce.ac.in>
2017-10-23 16:03:42 +0530

    Create ReverseInteger.java

---
+++ ReverseInteger.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ReverseInteger {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int no=scanner.nextInt();
+		String num=""+no;
+    StringBuffer buffer = new StringBuffer(num);
+    num.reverse();
+		System.out.println(decimal);
+		scanner.close();
+	}
